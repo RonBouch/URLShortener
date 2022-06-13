0ca100ca1f80cdb541d9965b37e74046154dea33
rbouchnick <rbouchnick@isracard.co.il>
2022-06-13 11:37:42 +0300

    Run ReactJS and Nodejs in same terminal

--- urlshortenerapp/src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "./Header/Header";
-import LinkRedirect from "./LinkRedirect/LinkRedirect";
-import NotFoundPage from "./NotFoundPage/NotFoundPage";
-
-export {
-    Header,
-    LinkRedirect,
-    NotFoundPage,
-}
--- urlshortenerapp/src/screens/index.tsx
+++
@@ -1,7 +0,0 @@
-import NewURL from "./NewURL/NewURL";
-import AdminPage from "./AdminPage/AdminPage";
-
-export {
-    NewURL,
-    AdminPage,
-}
